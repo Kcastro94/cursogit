67495a800aa7725e82a4a66f69e0143d3aeb50ac
Cristina Elisabet Rodríguez Palmer <cristinarodriguez.palmer@gmail.com>
2016-03-29 21:30:35 +0200

    cambiado el prueba

--- prueba.java
+++ prueba.java
@@ -2,7 +2,7 @@
 public class prueba {
 	private static String nombre = "Kevin";
 	public static void main (String [] args){
-		System.out.println("Hola " +nombre);
+		System.out.println("Taluego " +nombre);
 	}
  
 }
